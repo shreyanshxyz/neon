f24a1c85163def1e75bba848f02b38196f63b407
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-01-15 13:11:46 +0530

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 5173,
+  },
+});
