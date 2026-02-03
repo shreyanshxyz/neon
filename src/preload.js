5a6e671e760f3311d8c2f0a06867baf7575be354
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-03 19:02:55 +0530

    add file operations to preload API

--- src/preload.js
+++ src/preload.js
@@ -1,11 +1,17 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('process', {
-  env: process.env
+  env: process.env,
 });
 
 contextBridge.exposeInMainWorld('filesystem', {
   readDir: (path) => ipcRenderer.invoke('filesystem:readDir', path),
   getStats: (path) => ipcRenderer.invoke('filesystem:getStats', path),
   homePath: () => ipcRenderer.invoke('filesystem:homePath'),
+  readFile: (path) => ipcRenderer.invoke('filesystem:readFile', path),
+  writeFile: (path, content) => ipcRenderer.invoke('filesystem:writeFile', path, content),
+  copy: (source, dest) => ipcRenderer.invoke('filesystem:copy', source, dest),
+  move: (source, dest) => ipcRenderer.invoke('filesystem:move', source, dest),
+  delete: (path) => ipcRenderer.invoke('filesystem:delete', path),
+  createFolder: (path) => ipcRenderer.invoke('filesystem:createFolder', path),
 });
