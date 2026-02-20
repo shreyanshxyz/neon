d6d28c850a8d39fa7a5687fa4b63fd21004360d6
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2026-02-20 13:12:12 +0530

    Update entrypoint for index.html

--- src/main.ts
+++ src/main.ts
@@ -398,7 +398,7 @@ function createWindow(): void {
     mainWindow.loadURL('http://localhost:5173');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile('index.html');
+    mainWindow.loadFile(path.join(__dirname, 'index.html'));
   }
 
   mainWindow.on('closed', () => {
